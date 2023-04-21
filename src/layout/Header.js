4369ddf688f6e729cb1c923f6286b178b0ef6296
Siddharth <officialsiddharthbisht@gmail.com>
2023-04-21 10:14:37 +0530

    feat: used useNavigate , removed href in header

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,17 +1,57 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Header() {
+
+    const navigate = useNavigate();
+
     return (
         <section id="header">
                 <a href="#"> <img src="img/logo.png" className="logo" alt /></a>
                 <div>
                     <ul id="navbar">
-                        <li><a className="active " href="index.html">Home</a></li>
-                        <li><a href="shop.html">Shop</a></li>
-                        <li><a href="about.html">About</a></li>
-                        <li><a href="contact.html">Contact</a></li>
-                        <li><a href="login.html">Login / Sign Up</a></li>
-                        <li><a href="cart.html"><i className="fa-solid fa-bag-shopping" style={{ color: '#333c39' }} /></a></li>
+                        <li>
+                            <a className="active" 
+                        onClick={()=>{
+                            navigate("/")
+                        }}
+                        >
+                            Home
+                            </a>
+                        </li>
+                        <li>
+                            <a 
+                            onClick={()=>{
+                                navigate("/shop")
+                            }}
+                            >Shop</a>
+                        </li>
+                        <li>
+                            <a 
+                            onClick={()=>{
+                                navigate("/")
+                            }}
+                            >About</a>
+                        </li>
+                        <li>
+                            <a
+                            onClick={()=>{
+                                navigate("/")
+                            }}
+                            >Contact</a>
+                        </li>
+                        <li>
+                            <a 
+                            onClick={()=>{
+                                navigate("/")
+                            }}
+                            >Login / Sign Up</a>
+                        </li>
+                        <li>
+                            <a href="cart.html">
+                                <i className="fa-solid fa-bag-shopping" style={{ color: '#333c39' }} />
+                            </a>
+                        </li>
                         <a href="#" id="close"><i className="far fa-times" /></a>
                     </ul>
                 </div>
