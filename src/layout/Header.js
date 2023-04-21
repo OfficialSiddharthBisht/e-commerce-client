0800eaec44c12aed7f961c7c7ec34aaf306636fa
Siddharth <officialsiddharthbisht@gmail.com>
2023-04-21 09:58:19 +0530

    feat: header layout

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 function Header() {
     return (
-        <div>Header</div>
+        <section id="header">
+                <a href="#"> <img src="img/logo.png" className="logo" alt /></a>
+                <div>
+                    <ul id="navbar">
+                        <li><a className="active " href="index.html">Home</a></li>
+                        <li><a href="shop.html">Shop</a></li>
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                        <li><a href="login.html">Login / Sign Up</a></li>
+                        <li><a href="cart.html"><i className="fa-solid fa-bag-shopping" style={{ color: '#333c39' }} /></a></li>
+                        <a href="#" id="close"><i className="far fa-times" /></a>
+                    </ul>
+                </div>
+                <div id="mobile">
+                    <a href="cart.html"><i className="fa-thin fa-bag-shopping" /></a>
+                    <span id="bar"> <i className="fas fa-outdent" /></span>
+                </div>
+            </section>
     )
 }
 
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,24 +3,6 @@ import React from 'react'
 function Home() {
     return (
         <div>
-            <section id="header">
-                <a href="#"> <img src="img/logo.png" className="logo" alt /></a>
-                <div>
-                    <ul id="navbar">
-                        <li><a className="active " href="index.html">Home</a></li>
-                        <li><a href="shop.html">Shop</a></li>
-                        <li><a href="about.html">About</a></li>
-                        <li><a href="contact.html">Contact</a></li>
-                        <li><a href="login.html">Login / Sign Up</a></li>
-                        <li><a href="cart.html"><i className="fa-solid fa-bag-shopping" style={{ color: '#333c39' }} /></a></li>
-                        <a href="#" id="close"><i className="far fa-times" /></a>
-                    </ul>
-                </div>
-                <div id="mobile">
-                    <a href="cart.html"><i className="fa-thin fa-bag-shopping" /></a>
-                    <span id="bar"> <i className="fas fa-outdent" /></span>
-                </div>
-            </section>
             <section id="hero">
                 <h4>Trade-in-offer</h4>
                 <h2>Super value deals</h2>
