21dbdaf83b3a0635cda64630f5f0e59d68bed3ff
Siddharth <officialsiddharthbisht@gmail.com>
2023-04-26 15:33:53 +0530

    feat: redux setup initialized - store

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import {Provider} from 'react-redux';
+import store from "./store";
 
 const root = document.getElementById('root');
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store} >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   root
 );
 
